40cc5d00a8965fcd44eb8384b04e3deb72339f31
Jackwyf2019 <1779626055@qq.com>
2021-02-18 17:48:17 +0800

    redis实现分布式锁

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,11 +1,17 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.atguigu.gulimall.product.service.CategoryBrandRelationService;
 import com.atguigu.gulimall.product.vo.Catalog2Vo;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -25,6 +31,9 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     @Autowired
     private CategoryBrandRelationService categoryBrandRelationService;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -102,12 +111,81 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return categoryEntities;
     }
 
+    //TODO 产生堆外内存溢出
     /**
-     * 查出所有分类并封装成3级分类
+     * 缓存3级分类
      * @return
      */
     @Override
     public Map<String, List<Catalog2Vo>> getCatalogJson() {
+        //redis缓存中存储的是json字符串
+        String catalogJSON = redisTemplate.opsForValue().get("catalogJSON");
+        if(StringUtils.isEmpty(catalogJSON)){
+            //如果缓存中没有，查询数据库并加入缓存
+            Map<String, List<Catalog2Vo>> catalogJsonFromDB = null;
+            try {
+                catalogJsonFromDB = getCatalogJsonFromDBWithRedisLock();
+            } catch (InterruptedException e) {
+                log.error("查询数据库出错");
+            }
+            String s = JSON.toJSONString(catalogJsonFromDB);
+            redisTemplate.opsForValue().set("catalogJSON",s);
+            return catalogJsonFromDB;
+        }else{
+            Map<String, List<Catalog2Vo>> result = JSON.parseObject(catalogJSON, new TypeReference<Map<String, List<Catalog2Vo>>>() {});
+            return result;
+        }
+
+    }
+
+    /**
+     * Redis使用分布式锁
+     * @return
+     */
+    public Map<String, List<Catalog2Vo>> getCatalogJsonFromDBWithRedisLock() throws InterruptedException {
+
+        //上锁
+        String uuid = UUID.randomUUID().toString();//为保证在分布式系统中确认是自己的锁
+        Boolean lock = redisTemplate.opsForValue().setIfAbsent("lock", uuid, 300, TimeUnit.SECONDS);//原子性操作，只有一个线程能执行
+        if(lock){
+            //得到锁并加锁成功
+            Map<String, List<Catalog2Vo>> catalogJsonFromDB;
+            try {
+                //执行操作
+                catalogJsonFromDB = getCatalogJsonFromDB();
+            }finally {
+                //无论是否发生异常都要解锁
+                //lua脚本原子性操作
+                String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+                redisTemplate.execute(new DefaultRedisScript<Long>(script, Long.class), Collections.singletonList("lock"), uuid);
+            }
+            return catalogJsonFromDB;
+
+        }else{
+            //自旋等待锁
+            Thread.sleep(100);
+            return getCatalogJsonFromDBWithRedisLock();
+        }
+
+    }
+
+
+
+
+
+    /**
+     * 从数据库中查出所有分类并封装成3级分类
+     * @return
+     */
+    public Map<String, List<Catalog2Vo>> getCatalogJsonFromDB() {
+
+        //查数据库前先查缓存
+        String catalogJSON = redisTemplate.opsForValue().get("catalogJSON");
+        if (!StringUtils.isEmpty(catalogJSON)) {
+            Map<String, List<Catalog2Vo>> result = JSON.parseObject(catalogJSON, new TypeReference<Map<String, List<Catalog2Vo>>>() {
+            });
+            return result;
+        }
 
         //查出所有数据，用空间换时间
         List<CategoryEntity> selectList = baseMapper.selectList(null);
